5c7062b2c93cffd7952dfeb06403c9204fc13327
sravittxof <travis.fox90@gmail.come>
2021-04-03 22:42:09 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+
+var customerName = "bob";
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();    
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer(newBest) {
+    bestCustomer = newBest;
+}
+
+const leastFavoriteCustomer = "tom";
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'bob';
+}
